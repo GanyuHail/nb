b51b18bcd13fe4d56237bd80a06c605472a3b94d
HailGanyu <henryyanguk@gmail.com>
2022-10-17 21:42:47 +0100

    Updates

--- assets/index.40fb86a5.js
+++ assets/index.40fb86a5.js
@@ -16235,7 +16235,7 @@ function PlaneBufferGeometry(width, height, widthSegments, heightSegments) {
 }
 PlaneBufferGeometry.prototype = Object.create(BufferGeometry.prototype);
 PlaneBufferGeometry.prototype.constructor = PlaneBufferGeometry;
-function WebGLBackground(renderer, state, objects, premultipliedAlpha) {
+function WebGLBackground(renderer, state, objects2, premultipliedAlpha) {
   var clearColor = new Color(0);
   var clearAlpha = 0;
   var planeMesh;
@@ -16284,7 +16284,7 @@ function WebGLBackground(renderer, state, objects, premultipliedAlpha) {
             return this.uniforms.tCube.value;
           }
         });
-        objects.update(boxMesh);
+        objects2.update(boxMesh);
       }
       var texture = background.isWebGLRenderTargetCube ? background.texture : background;
       boxMesh.material.uniforms.tCube.value = texture;
@@ -16313,7 +16313,7 @@ function WebGLBackground(renderer, state, objects, premultipliedAlpha) {
             return this.uniforms.t2D.value;
           }
         });
-        objects.update(planeMesh);
+        objects2.update(planeMesh);
       }
       planeMesh.material.uniforms.t2D.value = background;
       if (background.matrixAutoUpdate === true) {
@@ -20921,7 +20921,7 @@ function WebGLRenderer(parameters) {
     throw error;
   }
   var extensions, capabilities, state, info;
-  var properties, textures, attributes, geometries, objects;
+  var properties, textures, attributes, geometries, objects2;
   var programCache, renderLists, renderStates;
   var background, morphtargets, bufferRenderer, indexedBufferRenderer;
   var utils;
@@ -20947,12 +20947,12 @@ function WebGLRenderer(parameters) {
     textures = new WebGLTextures(_gl, extensions, state, properties, capabilities, utils, info);
     attributes = new WebGLAttributes(_gl);
     geometries = new WebGLGeometries(_gl, attributes, info);
-    objects = new WebGLObjects(_gl, geometries, attributes, info);
+    objects2 = new WebGLObjects(_gl, geometries, attributes, info);
     morphtargets = new WebGLMorphtargets(_gl);
     programCache = new WebGLPrograms(_this, extensions, capabilities);
     renderLists = new WebGLRenderLists();
     renderStates = new WebGLRenderStates();
-    background = new WebGLBackground(_this, state, objects, _premultipliedAlpha);
+    background = new WebGLBackground(_this, state, objects2, _premultipliedAlpha);
     bufferRenderer = new WebGLBufferRenderer(_gl, extensions, info, capabilities);
     indexedBufferRenderer = new WebGLIndexedBufferRenderer(_gl, extensions, info, capabilities);
     info.programs = programCache.programs;
@@ -20967,7 +20967,7 @@ function WebGLRenderer(parameters) {
   var vr = typeof navigator !== "undefined" && "xr" in navigator ? new WebXRManager(_this, _gl) : new WebVRManager(_this);
   this.vr = vr;
   var multiview = new WebGLMultiview(_this, _gl);
-  var shadowMap = new WebGLShadowMap(_this, objects, capabilities.maxTextureSize);
+  var shadowMap = new WebGLShadowMap(_this, objects2, capabilities.maxTextureSize);
   this.shadowMap = shadowMap;
   this.getContext = function() {
     return _gl;
@@ -21103,7 +21103,7 @@ function WebGLRenderer(parameters) {
     renderLists.dispose();
     renderStates.dispose();
     properties.dispose();
-    objects.dispose();
+    objects2.dispose();
     vr.dispose();
     animation.stop();
   };
@@ -21508,7 +21508,7 @@ function WebGLRenderer(parameters) {
           if (sortObjects) {
             _vector3.setFromMatrixPosition(object.matrixWorld).applyMatrix4(_projScreenMatrix);
           }
-          var geometry = objects.update(object);
+          var geometry = objects2.update(object);
           var material = object.material;
           if (material.visible) {
             currentRenderList.push(object, geometry, material, groupOrder, _vector3.z, null);
@@ -21530,7 +21530,7 @@ function WebGLRenderer(parameters) {
           if (sortObjects) {
             _vector3.setFromMatrixPosition(object.matrixWorld).applyMatrix4(_projScreenMatrix);
           }
-          var geometry = objects.update(object);
+          var geometry = objects2.update(object);
           var material = object.material;
           if (Array.isArray(material)) {
             var groups = geometry.groups;
@@ -31356,23 +31356,23 @@ function AnimationObjectGroup() {
 Object.assign(AnimationObjectGroup.prototype, {
   isAnimationObjectGroup: true,
   add: function() {
-    var objects = this._objects, nObjects = objects.length, nCachedObjects = this.nCachedObjects_, indicesByUUID = this._indicesByUUID, paths = this._paths, parsedPaths = this._parsedPaths, bindings = this._bindings, nBindings = bindings.length, knownObject = void 0;
+    var objects2 = this._objects, nObjects = objects2.length, nCachedObjects = this.nCachedObjects_, indicesByUUID = this._indicesByUUID, paths = this._paths, parsedPaths = this._parsedPaths, bindings = this._bindings, nBindings = bindings.length, knownObject = void 0;
     for (var i = 0, n2 = arguments.length; i !== n2; ++i) {
       var object = arguments[i], uuid = object.uuid, index2 = indicesByUUID[uuid];
       if (index2 === void 0) {
         index2 = nObjects++;
         indicesByUUID[uuid] = index2;
-        objects.push(object);
+        objects2.push(object);
         for (var j = 0, m2 = nBindings; j !== m2; ++j) {
           bindings[j].push(new PropertyBinding(object, paths[j], parsedPaths[j]));
         }
       } else if (index2 < nCachedObjects) {
-        knownObject = objects[index2];
-        var firstActiveIndex = --nCachedObjects, lastCachedObject = objects[firstActiveIndex];
+        knownObject = objects2[index2];
+        var firstActiveIndex = --nCachedObjects, lastCachedObject = objects2[firstActiveIndex];
         indicesByUUID[lastCachedObject.uuid] = index2;
-        objects[index2] = lastCachedObject;
+        objects2[index2] = lastCachedObject;
         indicesByUUID[uuid] = firstActiveIndex;
-        objects[firstActiveIndex] = object;
+        objects2[firstActiveIndex] = object;
         for (var j = 0, m2 = nBindings; j !== m2; ++j) {
           var bindingsForPath = bindings[j], lastCached = bindingsForPath[firstActiveIndex], binding = bindingsForPath[index2];
           bindingsForPath[index2] = lastCached;
@@ -31381,22 +31381,22 @@ Object.assign(AnimationObjectGroup.prototype, {
           }
           bindingsForPath[firstActiveIndex] = binding;
         }
-      } else if (objects[index2] !== knownObject) {
+      } else if (objects2[index2] !== knownObject) {
         console.error("THREE.AnimationObjectGroup: Different objects with the same UUID detected. Clean the caches or recreate your infrastructure when reloading scenes.");
       }
     }
     this.nCachedObjects_ = nCachedObjects;
   },
   remove: function() {
-    var objects = this._objects, nCachedObjects = this.nCachedObjects_, indicesByUUID = this._indicesByUUID, bindings = this._bindings, nBindings = bindings.length;
+    var objects2 = this._objects, nCachedObjects = this.nCachedObjects_, indicesByUUID = this._indicesByUUID, bindings = this._bindings, nBindings = bindings.length;
     for (var i = 0, n2 = arguments.length; i !== n2; ++i) {
       var object = arguments[i], uuid = object.uuid, index2 = indicesByUUID[uuid];
       if (index2 !== void 0 && index2 >= nCachedObjects) {
-        var lastCachedIndex = nCachedObjects++, firstActiveObject = objects[lastCachedIndex];
+        var lastCachedIndex = nCachedObjects++, firstActiveObject = objects2[lastCachedIndex];
         indicesByUUID[firstActiveObject.uuid] = index2;
-        objects[index2] = firstActiveObject;
+        objects2[index2] = firstActiveObject;
         indicesByUUID[uuid] = lastCachedIndex;
-        objects[lastCachedIndex] = object;
+        objects2[lastCachedIndex] = object;
         for (var j = 0, m2 = nBindings; j !== m2; ++j) {
           var bindingsForPath = bindings[j], firstActive = bindingsForPath[lastCachedIndex], binding = bindingsForPath[index2];
           bindingsForPath[index2] = firstActive;
@@ -31407,18 +31407,18 @@ Object.assign(AnimationObjectGroup.prototype, {
     this.nCachedObjects_ = nCachedObjects;
   },
   uncache: function() {
-    var objects = this._objects, nObjects = objects.length, nCachedObjects = this.nCachedObjects_, indicesByUUID = this._indicesByUUID, bindings = this._bindings, nBindings = bindings.length;
+    var objects2 = this._objects, nObjects = objects2.length, nCachedObjects = this.nCachedObjects_, indicesByUUID = this._indicesByUUID, bindings = this._bindings, nBindings = bindings.length;
     for (var i = 0, n2 = arguments.length; i !== n2; ++i) {
       var object = arguments[i], uuid = object.uuid, index2 = indicesByUUID[uuid];
       if (index2 !== void 0) {
         delete indicesByUUID[uuid];
         if (index2 < nCachedObjects) {
-          var firstActiveIndex = --nCachedObjects, lastCachedObject = objects[firstActiveIndex], lastIndex = --nObjects, lastObject = objects[lastIndex];
+          var firstActiveIndex = --nCachedObjects, lastCachedObject = objects2[firstActiveIndex], lastIndex = --nObjects, lastObject = objects2[lastIndex];
           indicesByUUID[lastCachedObject.uuid] = index2;
-          objects[index2] = lastCachedObject;
+          objects2[index2] = lastCachedObject;
           indicesByUUID[lastObject.uuid] = firstActiveIndex;
-          objects[firstActiveIndex] = lastObject;
-          objects.pop();
+          objects2[firstActiveIndex] = lastObject;
+          objects2.pop();
           for (var j = 0, m2 = nBindings; j !== m2; ++j) {
             var bindingsForPath = bindings[j], lastCached = bindingsForPath[firstActiveIndex], last = bindingsForPath[lastIndex];
             bindingsForPath[index2] = lastCached;
@@ -31426,10 +31426,10 @@ Object.assign(AnimationObjectGroup.prototype, {
             bindingsForPath.pop();
           }
         } else {
-          var lastIndex = --nObjects, lastObject = objects[lastIndex];
+          var lastIndex = --nObjects, lastObject = objects2[lastIndex];
           indicesByUUID[lastObject.uuid] = index2;
-          objects[index2] = lastObject;
-          objects.pop();
+          objects2[index2] = lastObject;
+          objects2.pop();
           for (var j = 0, m2 = nBindings; j !== m2; ++j) {
             var bindingsForPath = bindings[j];
             bindingsForPath[index2] = bindingsForPath[lastIndex];
@@ -31444,14 +31444,14 @@ Object.assign(AnimationObjectGroup.prototype, {
     var indicesByPath = this._bindingsIndicesByPath, index2 = indicesByPath[path], bindings = this._bindings;
     if (index2 !== void 0)
       return bindings[index2];
-    var paths = this._paths, parsedPaths = this._parsedPaths, objects = this._objects, nObjects = objects.length, nCachedObjects = this.nCachedObjects_, bindingsForPath = new Array(nObjects);
+    var paths = this._paths, parsedPaths = this._parsedPaths, objects2 = this._objects, nObjects = objects2.length, nCachedObjects = this.nCachedObjects_, bindingsForPath = new Array(nObjects);
     index2 = bindings.length;
     indicesByPath[path] = index2;
     paths.push(path);
     parsedPaths.push(parsedPath);
     bindings.push(bindingsForPath);
-    for (var i = nCachedObjects, n2 = objects.length; i !== n2; ++i) {
-      var object = objects[i];
+    for (var i = nCachedObjects, n2 = objects2.length; i !== n2; ++i) {
+      var object = objects2[i];
       bindingsForPath[i] = new PropertyBinding(object, path, parsedPath);
     }
     return bindingsForPath;
@@ -32212,14 +32212,14 @@ Object.assign(Raycaster.prototype, {
     intersects2.sort(ascSort);
     return intersects2;
   },
-  intersectObjects: function(objects, recursive, optionalTarget) {
+  intersectObjects: function(objects2, recursive, optionalTarget) {
     var intersects2 = optionalTarget || [];
-    if (Array.isArray(objects) === false) {
+    if (Array.isArray(objects2) === false) {
       console.warn("THREE.Raycaster.intersectObjects: objects is not an Array.");
       return intersects2;
     }
-    for (var i = 0, l2 = objects.length; i < l2; i++) {
-      intersectObject(objects[i], this, intersects2, recursive);
+    for (var i = 0, l2 = objects2.length; i < l2; i++) {
+      intersectObject(objects2[i], this, intersects2, recursive);
     }
     intersects2.sort(ascSort);
     return intersects2;
@@ -35159,7 +35159,6 @@ reactJsxRuntime_production_min.jsxs = q;
 const jsx = jsxRuntime.exports.jsx;
 function App() {
   react.exports.useEffect(() => {
-    let objects = [];
     const scene = new Scene();
     const fov2 = 75;
     const aspect2 = 2;
@@ -35172,6 +35171,7 @@ function App() {
       canvas
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
     const ambientLight = new AmbientLight(16761035, 2);
     ambientLight.castShadow = true;
     ambientLight.physicallyCorrectLights = true;
@@ -35195,19 +35195,6 @@ function App() {
     paintGeometry.userData = {
       URL: "https://github.com/GanyuHail/nb/blob/main/src/weOpMin.jpg"
     };
-    document.addEventListener("mousedown", onMouseDown);
-    function onMouseDown(event) {
-      event.preventDefault();
-      const mouse3D = new Vector3(event.clientX / window.innerWidth * 2 - 1, -(event.clientY / window.innerheight) * 2 - 1, 0.5);
-      const raycaster = new Raycaster();
-      raycaster.setFromCamera(mouse3D, camera);
-      const intersects2 = raycaster.intersectObjects(objects, true);
-      if (intersects2.length > 0) {
-        console.log("click!");
-        intersects2[0].object.material.color.setHex(Math.random() * 16777215);
-      }
-    }
-    document.getElementById("container").appendChild(renderer.domElement);
     const controls = new OrbitControls(camera, renderer.domElement);
     const animate = () => {
       controls.update();
