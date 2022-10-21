9a2a0fcbfd68bf3bfcb418b95a6d41c933f64cb3
HailGanyu <henryyanguk@gmail.com>
2022-10-21 11:36:27 +0100

    dist page 2 vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/<3d>/',
   build: {
-    minify: false
+    minify: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'page2.html')
+      }
+    }
   }
 })
