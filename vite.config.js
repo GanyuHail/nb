6fc5e23c5ac9abf73357a2a4e48a96f14be5f542
HailGanyu <henryyanguk@gmail.com>
2022-11-02 14:16:30 +0000

    added js files to dist

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,8 @@ export default defineConfig({
         nested3: resolve(__dirname, 'page3.html'),
         nested4: resolve(__dirname, 'dropDown.js'),
         nested5: resolve(__dirname, 'page4.html'),
-        nested6: resolve(__dirname, 'paypal.js')
+        nested6: resolve(__dirname, 'paypal.js'),
+        nested7: resolve(__dirname, 'server.js')
       }
     }
   }
