df8a435ade9a685e1308a668612151c5adb9f0a8
HailGanyu <henryyanguk@gmail.com>
2022-11-08 17:23:27 +0000

    Updates

---
+++ assets/nested3.94433c1a.js
@@ -0,0 +1,40 @@
+import { c as client, j as jsx, R as React } from "./jsx-runtime.94186190.js";
+/* empty css                  */paypal.Buttons({
+  createOrder: function() {
+    return fetch("/create-order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        items: [{
+          id: 1,
+          quantity: 2
+        }, {
+          id: 2,
+          quantity: 3
+        }]
+      })
+    }).then((res) => {
+      if (res.ok)
+        return res.json();
+      return res.json().then((json) => Promise.reject(json));
+    }).then(({
+      id
+    }) => {
+      return id;
+    }).catch((e) => {
+      console.error(e.error);
+    });
+  },
+  onApprove: function(data, actions) {
+    return actions.order.capture().then(function(details) {
+      alert("Transaction completed by " + details.payer.name.given_name);
+    });
+  }
+}).render("#paypal");
+function StoreApp() {
+}
+client.createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsx(React.StrictMode, {
+  children: /* @__PURE__ */ jsx(StoreApp, {})
+}));
---
+++ assets/nested6.f4ddd870.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("src"));
+app.use(express.json());
+const paypal = require("@paypal/checkout-server-sdk");
+const Environment = paypal.core.LiveEnvironment;
+const paypalClient = new paypal.core.PayPalHttpClient(new Environment(process.env.PAYPAL_CLIENT_ID, process.env.PAYPAL_CLIENT_SECRET));
+const storeItems = /* @__PURE__ */ new Map([
+  [1, { price: 100, name: "Learn React Today" }],
+  [2, { price: 200, name: "Learn CSS Today" }]
+]);
+app.get("/", (req, res) => {
+  res.render("index", {
+    paypalClientId: process.env.PAYPAL_CLIENT_ID
+  });
+});
+app.post("/create-order", async (req, res) => {
+  const request = new paypal.orders.OrdersCreateRequest();
+  const total = req.body.items.reduce((sum, item) => {
+    return sum + storeItems.get(item.id).price * item.quantity;
+  }, 0);
+  request.prefer("return=representation");
+  request.requestBody({
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "GBP",
+          value: total,
+          breakdown: {
+            item_total: {
+              currency_code: "GBP",
+              value: total
+            }
+          }
+        },
+        items: req.body.items.map((item) => {
+          const storeItem = storeItems.get(item.id);
+          return {
+            name: storeItem.name,
+            unit_amount: {
+              currency_code: "GBP",
+              value: storeItem.price
+            },
+            quantity: item.quantity
+          };
+        })
+      }
+    ]
+  });
+  try {
+    const order = await paypalClient.execute(request);
+    res.json({ id: order.result.id });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+app.listen(5173);
