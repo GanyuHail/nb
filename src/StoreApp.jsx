d3760a9e45180c151285863044ac678c5d45abfd
HailGanyu <henryyanguk@gmail.com>
2022-11-08 17:23:44 +0000

    push compiled store code

--- src/StoreApp.jsx
+++ src/StoreApp.jsx
@@ -1,29 +1,39 @@
-// //@ts-check
-
-import { useEffect } from "react";
+////@ts-check
 
 paypal.Buttons({
-  createOrder: function(data, actions) {
-    // This function sets up the details of the transaction, including the amount and line item details.
-    return actions.order.create({
-      purchase_units: [{
-        amount: {
-          value: '0.01'
-        }
-      }]
-    });
+  createOrder: function () {
+    return fetch("/create-order", {
+      method: "POST",
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify({
+        items: [
+          {
+            id: 1,
+            quantity: 2
+          },
+          { id: 2, quantity: 3 },
+        ],
+      }),
+    }).then(res => {
+      if (res.ok) return res.json()
+      return res.json().then(json => Promise.reject(json))
+    }).then(({ id }) => {
+      return id
+    }).catch(e => {
+      console.error(e.error)
+    })
   },
-  onApprove: function(data, actions) {
-    // This function captures the funds from the transaction.
-    return actions.order.capture().then(function(details) {
-      // This function shows a transaction success message to your buyer.
+  onApprove: function (data, actions) {
+    return actions.order.capture().then(function (details) {
       alert('Transaction completed by ' + details.payer.name.given_name);
     });
   }
 }).render('#paypal');
 
 function StoreApp() {
-  
+
   //This function displays payment buttons on your web page.
 };
 
